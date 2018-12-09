ebfdb06a80e717adab6c644453113e547e7810fd
steverodri91 <steve20.pro@gmail.com>
2018-12-08 20:51:52 -0500

    created GET ONE route

--- routes/places.js
+++ routes/places.js
@@ -10,6 +10,18 @@ placesRouter.get("/", async (req, res) => {
     });
   } catch (e) {
     console.log("Could not process request for GET places");
+    res.sendStatus(404);
+  }
+});
+
+placesRouter.get("/:id", async (req, res) => {
+  try {
+    const place = await Place.findByPk(req.params.id);
+    res.json({
+      place
+    });
+  } catch (e) {
+    console.log("Could not process request for GET ONE place");
     res.sendStatus(500);
   }
 });
