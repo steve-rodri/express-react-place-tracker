e27c0a1ba1e309108f32624d1cde424768fcf5ce
steverodri91 <steve20.pro@gmail.com>
2018-12-08 20:38:07 -0500

    init new Express App - updated package.json

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(bodyParser.json());
+app.use(logger("dev"));
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "This is the Root of the Places Tracker API"
+  });
+});
+
+app.listen(PORT, () => console.log(`Up and Running on Port: ${PORT}`));
